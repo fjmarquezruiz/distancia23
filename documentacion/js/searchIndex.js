321c47dec8b33fb61ec1fcaa69e035adcec3f727
fjmarquezruiz <fjmarquezruiz@gmail.com>
2023-05-15 19:15:21 +0200

    Subir carpeta documentación de Actividad 3
    Subida la carpeta documentación de la Actividad 3 de la Unidad 05

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Funci\u00F3n\u0020que\u0020realiza\u0020la\u0020suma\u0020de\u0020dos\u0020n\u00FAmeros.",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\restar\u0028\u0029",
+            "name": "restar",
+            "summary": "Funci\u00F3n\u0020que\u0020realiza\u0020la\u0020resta\u0020de\u0020dos\u0020n\u00FAmeros.",
+            "url": "namespaces/default.html#function_restar"
+        },                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020que\u0020implementa\u0020los\u0020c\u00E1lculos\u0020b\u00E1sicos\u0020de\u0020una\u0020calculadora",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Amultiplicar\u0028\u0029",
+            "name": "multiplicar",
+            "summary": "M\u00E9todo\u0020de\u0020la\u0020clase\u0020Calculadora\u0020que\u0020realiza\u0020la\u0020operaci\u00F3n\u0020de\u0020multiplicaci\u00F3n",
+            "url": "classes/Calculadora.html#method_multiplicar"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Adividir\u0028\u0029",
+            "name": "dividir",
+            "summary": "M\u00E9todo\u0020de\u0020la\u0020clase\u0020Calculadora\u0020que\u0020realiza\u0020la\u0020operaci\u00F3n\u0020de\u0020divisi\u00F3n",
+            "url": "classes/Calculadora.html#method_dividir"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
